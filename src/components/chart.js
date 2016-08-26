306c6bd2216516254f7ccaaa2183332b5268d203
Ross Dyson <ross@soundninja.com>
2016-08-26 03:25:31 -0700

    charts perfected

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+    let sum = 0;
+    data.forEach(function(number){
+        sum += number;
+    })
+    return Math.round(sum/data.length);
+}
 
 export default (props) => {
     return (
         <div>
-            <Sparklines data={props.data} height={120} width={180} >
+            <Sparklines svgWidth={240} svgHeight={160} data={props.data} >
                 <SparklinesLine color={props.color} />
+                <SparklinesReferenceLine type="avg" />
             </Sparklines>
+            <div>Avg: {average(props.data)} {props.units}</div>
         </div>
     )
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,11 +7,20 @@ class WeatherList extends React.Component {
     renderWeather (cityData) {
         // console.log(cityData);
         const temps = cityData.list.map((weather) => weather.main.temp);
+        const pressures = cityData.list.map((weather) => weather.main.pressure);
+        const humidities = cityData.list.map((weather) => weather.main.humidity);
+
         return (
             <tr key={cityData.city.name}>
                 <td>{cityData.city.name}</td>
                 <td>
-                    <Chart data={temps} color="red" />
+                    <Chart data={temps} color="red" units="K" />
+                </td>
+                <td>
+                    <Chart data={pressures} color="blue" units="hPa" />
+                </td>
+                <td>
+                    <Chart data={humidities} color="orange" units="%" />
                 </td>
             </tr>
         )
@@ -23,9 +32,9 @@ class WeatherList extends React.Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
